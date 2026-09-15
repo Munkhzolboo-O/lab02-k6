e0e9a002a304fc428cc6502b5fc0242c7694b58d
Munkhzolboo-O <zolboomnkh@gmail.com>
2026-09-15 13:38:08 +0800

    Prepare baseline k6 test

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ import { check, sleep } from 'k6';
 export const options = {
   vus: 5,
   duration: '30s',
-
-  thresholds: {
-    http_req_duration: ['p(95)<300'],
-    http_req_failed: ['rate<0.01'],
-  },
 };
 
 export default function () {
