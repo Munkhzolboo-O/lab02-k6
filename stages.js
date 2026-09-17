34826678d403d15f66cdef10d61d39f203d6a8f5
Munkhzolboo-O <zolboomnkh@gmail.com>
2026-09-17 21:53:36 +0800

    Add stages load test script

---
+++ stages.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '15s', target: 5 },
+    { duration: '15s', target: 30 },
+    { duration: '15s', target: 100 },
+    { duration: '15s', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io/');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
